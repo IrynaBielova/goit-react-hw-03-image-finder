f9d08356a9232595ddd4668dc8504629c4040cc7
IrynaBielova <wintergirl@i.ua>
2022-11-21 23:26:43 +0200

    loader_change

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -8,9 +8,7 @@ export const Loader = () => {
     width = "80"
     radius = "9"
     color = 'green'
-    ariaLabel = 'three-dots-loading'     
-    wrapperStyle
-    wrapperClass
+    ariaLabel = 'Loading'     
   />
   )
 }
